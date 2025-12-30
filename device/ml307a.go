c61214c3eccbfbf54cfe96ccde76d969da8965e1
若海 <wang@rehiy.com>
2025-12-30 09:24:04 +0000

    feat: 添加 ML307A 设备支持

---
+++ device/ml307a.go
@@ -0,0 +1,23 @@
+package device
+
+import (
+	"github.com/rehiy/modem/at"
+)
+
+type ML307A struct {
+	CommandSet      *at.CommandSet
+	ResponseSet     *at.ResponseSet
+	NotificationSet *at.NotificationSet
+}
+
+func NewML307A() *ML307A {
+	commandSet := at.DefaultCommandSet()
+	responseSet := at.DefaultResponseSet()
+	notificationSet := at.DefaultNotificationSet()
+
+	return &ML307A{
+		CommandSet:      commandSet,
+		ResponseSet:     responseSet,
+		NotificationSet: notificationSet,
+	}
+}
